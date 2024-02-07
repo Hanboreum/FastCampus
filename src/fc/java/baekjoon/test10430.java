38db3f2f5a9024f01c12a6b0cf477211e35d781c
fullm <matoma1_@naver.com>
2024-02-07 15:22:08 +0900

    Day1 0207 baekjoon

---
+++ src/fc/java/baekjoon/test10430.java
@@ -0,0 +1,22 @@
+package fc.java.baekjoon;
+import java.util.*;
+
+public class test10430 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+
+            sc.close();
+
+            System.out.println((A + B) % C);
+            System.out.println((A % C + B % C) % C);
+            System.out.println((A * B) % C);
+            System.out.println((A % C * B % C) % C);
+
+        }
+    }
+
+
