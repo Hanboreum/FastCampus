0dd0579b50f2c7d60b333fe52cbeb3a8c8b7bca2
fullm <matoma1_@naver.com>
2024-02-13 23:55:30 +0900

    20230213 17427

---
+++ src/fc/java/bytest/ct17427.java
@@ -0,0 +1,23 @@
+package fc.java.bytest;
+
+import java.util.Scanner;
+
+public class ct17427 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); //입력받은 자연수
+        long sum = 0; // 약수의 합을 저장할 공간.1000000까지 입력할 것을 고려
+
+        //1부터 n가지의 각 자연수 i에 대해
+        for(int i = 1; i <= N; i++) {
+            //i의 배수들의 합을 구하여 sum에 더함
+            sum += (N/i) * i; //N을 i로 나눈 몫을 구함. 이는 i의 배수의 개수
+            //이를 다시 i와 곱해 i의 배수들의 합을 구함
+            //이렇게 구한 값을 sum에 저장
+        }
+        System.out.println(sum);
+        //반복문을 통해 1부터 N까지의 각 자연수 i에 대해 i의 배수들의 합을 계산.
+        //이를 누적하여 sum에 저징
+        //이 과정을 통해 1부터 N까지의 모든 자연수에 대한 약수의 합을 구할 수 있다.
+    }
+}
--- src/fc/java/bytest/test.java
+++ src/fc/java/bytest/test.java
@@ -6,27 +6,13 @@ public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] divisors = new int[N];
+        int n = sc.nextInt();
 
-        // 진짜 약수 입력받기
-        for (int i = 0; i < N; i++) {
-            divisors[i] = sc.nextInt();
+        long answer = 0;
+        for(int i = 1; i <= n; i++) {
+            answer += n / i * i;
         }
 
-        // 최대값과 최소값 찾기
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            if (divisors[i] > max) {
-                max = divisors[i];
-            }
-            if (divisors[i] < min) {
-                min = divisors[i];
-            }
-        }
-
-        // 결과 출력
-        System.out.println(max * min);
+        System.out.println(answer);
     }
 }
