2e705cbfd0c69e36d2d9f36890a9f9f15d43553f
fullm <matoma1_@naver.com>
2024-02-20 23:02:25 +0900

    20230220 인터넷강의 자바 DTO, VO, DAO, Utility

--- src/fc/java/part2/ScannerTest.java
+++ src/fc/java/part2/ScannerTest.java
@@ -3,25 +3,26 @@ package fc.java.part2;
 import java.util.*;
 public class ScannerTest {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print(" 정수입력");
-        int num = scan.nextInt();
-        System.out.println("num = " + num);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수입력");
+        int num = sc.nextInt();
+        System.out.println(num);
 
-        System.out.print(" 실수입력");
-        float f =scan.nextFloat();
-        System.out.println("f = " + f);
+        System.out.println("실수");
+        float fsc = sc.nextFloat();
+        System.out.println(fsc);
 
-        System.out.print("  문자입력");
-        String str = scan.next();
-        System.out.println("str = " + str);
+        System.out.println("공백 X문자열");
+        String ssc = sc.next();
+        System.out.println("ssc = " + ssc);
 
-        scan.nextLine();
+        sc.nextLine();//버퍼 비우기. 스트ㅡ림비우기
 
-        System.out.print("  문자열입력");
-        String str1 = scan.nextLine();
-        System.out.println("str = " + str1);
+        System.out.println("String");
+        String sssc = sc.nextLine();
+        System.out.println("sssc = " + sssc);
 
-        scan.close();
+        sc.close(); //연결 끊기
+
     }
 }
