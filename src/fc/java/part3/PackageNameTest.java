d177414f3649602bb507e731c2cdbbb29e7ad6b4
fullm <matoma1_@naver.com>
2024-02-21 12:35:41 +0900

    20230221 ch04잘설계된VO
    정보은닉하기전

--- src/fc/java/part3/PackageNameTest.java
+++ src/fc/java/part3/PackageNameTest.java
@@ -1,5 +1,5 @@
 package fc.java.part3;
-
+ //import java.lang.*; 이것이 생략되어있다.
 import fc.java.model.CarDAO;
 import fc.java.model.CarDTO;
 
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class PackageNameTest {
     public static void main(String[] args) {
-        String str;
+        String str; // String, Scanner, system 자바 제공.
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello world");
         CarDTO dto = new CarDTO();
