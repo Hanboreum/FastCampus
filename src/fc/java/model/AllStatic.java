602f71071b649db4b500c2bb0b5e61c14f0f9044
fullm <matoma1_@naver.com>
2024-02-23 18:59:07 +0900

    20240223 객체생성과 static의 관계

--- src/fc/java/model/AllStatic.java
+++ src/fc/java/model/AllStatic.java
@@ -1,15 +1,16 @@
 package fc.java.model;
 
-public class AllStatic {
-    private AllStatic(){}  //인위적인 생성자 생ㅇ성
-    public static int hap( int a, int b){
-        int v=a+b;
-        return v;
-    }
-    public static int max(int a, int b){
-        return a>b ? a:b;
-    }
-    public static int min (int a,int b){
-        return a<b? a:b;
-    }
+public class AllStatic { //utility class
+   public static int hap(int a, int b){
+       int v = a+b;
+       return  v;
+   }
+   public static int max(int a, int b){
+       return a>b? a:b;
+       //a가 b보다 크면 a를 넘기고
+   }
+   public static int min(int a, int b){
+       return a< b? a:b;
+       //a가 b보다 작음 b 넘기기
+   }
 }
--- src/fc/java/part3/AllStaticTest.java
+++ src/fc/java/part3/AllStaticTest.java
@@ -4,13 +4,7 @@ import fc.java.model.AllStatic;
 
 public class AllStaticTest {
     public static void main(String[] args) {
+        // 두수의 합, min. max
 
-        //AllStatic st = new AllStatic();
-        //System.out.println(st.hap(10,20));
-        //System.out.println(st.max(10,20));
-
-        //이게 더 바람직
-        System.out.println(AllStatic.hap(10,20));
-        System.out.println(AllStatic.max(20,409));
     }
 }
