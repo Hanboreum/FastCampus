6666a2b5f96b60b8048eb0baa51b36231f027b95
fullm <matoma1_@naver.com>
2024-02-26 17:19:11 +0900

    20240226 part3

--- src/fc/java/part3/NoneStaticTest.java
+++ src/fc/java/part3/NoneStaticTest.java
@@ -3,15 +3,14 @@ package fc.java.part3;
 public class NoneStaticTest {
     public static void main(String[] args) {
         int a=10;
-        int b=10;
+        int b=33;
+        //int sum =add(a,b); add가 스테틱이 아니라 호출 안됨
         NoneStaticTest st = new NoneStaticTest();
-        int sum = st.hap(a,b);
+        int sum = st.add(a,b);
         System.out.println("sum = " + sum);
-
     }
-    public int hap(int a, int b){
+    public int add(int a, int b){
         int v = a+b;
         return v;
-
     }
 }
