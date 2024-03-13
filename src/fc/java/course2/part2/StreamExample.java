def260235b4824aa2f2d079a811e9b7cadbec535
fullm <matoma1_@naver.com>
2024-03-13 11:52:13 +0900

    20240313
    메서드 참조

--- src/fc/java/course2/part2/StreamExample.java
+++ src/fc/java/course2/part2/StreamExample.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class StreamExample {
+    public static boolean isEven(int number){
+        return number % 2==0;
+    }
     public static void main(String[] args) {
         List<Integer>numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,9,10);
         Predicate<Integer> isEven = n->n%2 ==0;
         int sumOfSquares = numbers.stream()
-                .filter(isEven) //n->n % 2==0
+                .filter(StreamExample::isEven) //n->n % 2==0
                 .sorted()
                 .map(n -> n * n)
                 .reduce(0,Integer::sum);
